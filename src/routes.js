0ebc6c7b2d563cd76e6d2e52ad63f81208ff2adc
Marcin Bystrzycki <darkwave99@gmail.com>
2017-11-26 22:38:08 +0100

    initial commit

--- src/routes.js
+++ src/routes.js
@@ -4,13 +4,19 @@ import Navigation from './presentational/navigation.component.js';
 import Home from './presentational/home.component.js';
 import Contact from './presentational/contact.component.js';
 import NotFound from './presentational/not-found.component.js';
+import CountryFlagContainer from './containers/flag-container.component.js';
+import CountryDetailsContainer from './containers/country-details-container.component.js';
+import ContinentsContainer from './containers/continents-container.component.js';
 
 export default (
 	<div>
 		<Route path='/' component={Navigation} />
 		<Switch>
 			<Route exact path='/' component={Home} />
-			<Route path='/contact' component={Contact} />
+			<Route exact path='/countries' component={CountryFlagContainer} />
+			<Route path='/countries/country/:id' component={CountryDetailsContainer} />
+			<Route exact path='/continents' component={ContinentsContainer}/>
+			<Route exact path='/contact' component={Contact} />
 			<Route component={NotFound} />
 		</Switch>
 	</div>
